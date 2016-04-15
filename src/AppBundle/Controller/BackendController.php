b210196f0ff1058b6940025add1fa33c52568467
Yrii Pereskokov <skulines@mail.ru>
2016-04-15 07:24:40 +0500

    backend start

---
+++ src/AppBundle/Controller/BackendController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * AuthorizationController.php
+ *
+ * @author      Pereskokov Yurii
+ * @copyright   2016 Pereskokov Yurii
+ * @license     The MIT License (MIT) http://opensource.org/licenses/mit-license.php
+ * @link        https://github.com/pers1307/symfony-blog
+ */
+
+namespace pers1307\blog\AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+//use pers1307\blog\AppBundle\Entity\Article;
+use Symfony\Component\Routing\Annotation\Route;
+
+//use pers1307\blog\tests\AppBundle\Entity\KernelTestCase;
+
+class BackendController extends Controller
+{
+    /**
+     * @Route("/backend", name="authorization_index")
+     * @Method({"GET", "HEAD"})
+     *
+     * @return Response
+     */
+    public function indexAction()
+    {
+        //$content = 'test';
+        //$article = new Article();
+
+        //$article->setContent($content);
+
+        //$result = $article->getContent();
+
+        //$articleReposiory = $this->get('article_repository');
+        //$articles         = $articleReposiory->findAll();
+
+        //print_r($articles);
+
+//        $kernel = new KernelTestCase();
+//
+//        $t = $kernel->getKernel();
+//        $r = $kernel->getContainer();
+
+        //return new Response('<html><body>' . $result . '</body></html>');
+
+        //return new Response('<html><body>Форма авторизации</body></html>');
+
+        return $this->render('backend/backend.html.twig', []);
+    }
+}
